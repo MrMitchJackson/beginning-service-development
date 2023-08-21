f7ef42c2752edc5913a8d3921ade2ca1d285b7f3
Mitch Jackson <Taylorman42@gmeail.com>
2023-08-21 17:05:21 -0400

    End of day Monday

---
+++ BusinessClockSolution/BusinessClockApi/Program.cs
@@ -0,0 +1,43 @@
+using BusinessClockApi.Services;
+using BusinessClockApi;
+using static BusinessClockApi.Models.ClockModels;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<BusinessClock>();
+builder.Services.AddSingleton<ISystemTime, SystemTime>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapGet("/status", () =>
+{
+    var fakeResponse = new ClockResponseModel
+    {
+        IsOpen = true,
+        SupportContact = new SupportContactResponseModel
+        {
+            Name = "Mitch",
+            Phone = "800 555-1212",
+            Email = "mitch@company.com"
+        }
+    };
+    return Results.Ok(fakeResponse);
+});
+
+app.Run();
+
+public partial class Program { }
+
+
+
